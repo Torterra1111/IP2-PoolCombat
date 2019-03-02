d5973d344857cae512501f6e387ffe28785a6b21
Torterra1111 <44812405+Torterra1111@users.noreply.github.com>
2019-03-02 22:59:44 +0000

    Small update
    Fix a small error

--- Pool 2D/Assets/Scripts/SpikeScript.cs
+++ Pool 2D/Assets/Scripts/SpikeScript.cs
@@ -31,7 +31,6 @@ public class SpikeScript : MonoBehaviour
     void OnCollisionEnter2D(Collision2D dol)
     {
 
-        Debug.Log("AAA THINGS HURT");
         //if the tag is different from the collided object tag it runs the if statement 
         if (dol.gameObject.tag != gameObject.tag)
         {
