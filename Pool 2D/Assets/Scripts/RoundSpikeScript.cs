cc4f9c3c7a271e2dbd5cd52b085f86c5afd8d430
Torterra1111 <44812405+Torterra1111@users.noreply.github.com>
2019-03-27 22:05:25 +0000

    Changes aaaa
    Its kinda pwetty?

---
+++ Pool 2D/Assets/Scripts/RoundSpikeScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoundSpikeScript : MonoBehaviour
+{
+    Vector3 Waypoint;
+    Vector3 Waypoint2;
+    public GameObject WaypointT1;
+    public GameObject WaypointT2;
+
+    public bool travel = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Waypoint = WaypointT1.transform.position;
+        Waypoint2 = WaypointT2.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+        do
+        {
+            new WaitForSeconds(5.0f);
+            transform.position = Vector3.Lerp(Waypoint, Waypoint2, 2.0f);
+            transform.position = Vector3.Lerp(Waypoint2, Waypoint, 2.0f);
+
+        } while (travel);
+
+
+    }
+
+    void Travel1()
+    {
+        transform.position = Vector3.Lerp(Waypoint, Waypoint2, 2.0f);
+
+    }
+    void Travel2()
+    {
+        transform.position = Vector3.Lerp(Waypoint2, Waypoint, 2.0f);
+
+    }
+}
